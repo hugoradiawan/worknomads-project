c1c4de9d5ca25e71b826e9cad04c482538a892d6
hugoradiawan <hugoradiawan@gmail.com>
2025-08-31 06:46:30 +0700

    refactor(config): remove django admin urls
    - Remove the Django admin URL pattern from the authentication server's configuration.
    - Drop the corresponding django.contrib.admin import statement.
    - The admin interface is not required for the dedicated function of this authentication service.
    - This change simplifies the URL routing and reduces the overall attack surface.
    - Improves clarity by removing unused components from the core configuration.

--- backend/auth_server/auth_server/config/urls.py
+++ backend/auth_server/auth_server/config/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 
 from auth_server.services.register.register_view import RegisterView
@@ -6,7 +5,6 @@ from auth_server.services.auth.login_view import LoginView
 from auth_server.services.auth.token_refresh_view import CustomTokenRefreshView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/register", view=RegisterView.as_view(), name="register"),
     path("api/login", LoginView.as_view(), name="token_obtain_pair"),
     path("api/refreshtoken", CustomTokenRefreshView.as_view(), name="token_refresh"),
